5da0c9bacd7ce6717a685358b057bbbe0a792472
Raffaele Ausiello <raffaele.ausiello@eng.it>
2018-08-08 17:00:20 +0200

    Add project files.

---
+++ XMLQueryNestedApp2017Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLQueryNestedApp2017Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = GetFileXml(@"XmlFile\search.xml");
+            var x2 = x.SelectSingleNode("QUERY/CitySearch");
+        }
+        public static XmlDocument GetFileXml(string FileName)
+        {
+            try
+            {
+
+                string pathFile = FileName;
+                XmlDocument obj = new XmlDocument();
+                obj.Load(pathFile);
+                return obj;
+
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
